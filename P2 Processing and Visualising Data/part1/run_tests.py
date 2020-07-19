0809daf9ab0383227c0c8582100b65dabb847c96
alisonvpereira <alisonvpereira@gmail.com>
2020-07-19 11:59:34 +0800

    removed P2

--- P2 Processing and Visualising Data/part1/run_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from tests.part1_tests import Part1Tests
-
-runner = unittest.TextTestRunner()
-
-print('Running Tests...\n')
-runner.run(unittest.TestSuite((unittest.makeSuite(Part1Tests))))
